12d4f4ee67a65b382812cd8f7d25e7520256b148
JaneDo <jane.dolska@gmail.com>
2025-04-16 11:10:14 +0200

    Data split adjustment

--- ensemble_models/flat_ensemble.py
+++ ensemble_models/flat_ensemble.py
@@ -1377,8 +1377,9 @@ def train_ensemble_model(data, selected_features, model_filename='flat_stacked_e
     
     # 6) Train/Test split
     X_train, X_test, y_train, y_test = train_test_split(
-        X_resampled, y_resampled, test_size=0.2, random_state=42, stratify=y_clean
+        X_resampled, y_resampled, test_size=0.2, random_state=42, stratify=y_resampled
     )
+
     logging.info(f"Train size: X = {X_train.shape}, y = {y_train.shape}")
     logging.info(f"Test size: X = {X_test.shape}, y = {y_test.shape}")
     debug_target_presence(pd.concat([X_train, y_train], axis=1), "После разделения на выборки (flat)")
